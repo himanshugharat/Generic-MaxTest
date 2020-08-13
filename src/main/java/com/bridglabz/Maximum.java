62b76b9b0ae4877cc9e0e8abdf6cafd7dfc672e9
unknown <himanshugharat44@gmail.com>
2020-08-13 07:25:34 -0500

    ADDED:Get maximum from three float values

--- src/main/java/com/bridglabz/Maximum.java
+++ src/main/java/com/bridglabz/Maximum.java
@@ -1,18 +1,24 @@
 package com.bridglabz;
 
 
-
 public class Maximum {
-    int max=0;
     public Integer checkMaxInteger(Integer[] Integerarray) {
-        if(Integerarray[0].compareTo(Integerarray[1])>0 && Integerarray[0].compareTo(Integerarray[2])>0){
+        if (Integerarray[0].compareTo(Integerarray[1]) > 0 && Integerarray[0].compareTo(Integerarray[2]) > 0) {
             return Integerarray[0];
-        }
-        else if(Integerarray[1].compareTo(Integerarray[0])>0 && Integerarray[1].compareTo(Integerarray[2])>0) {
+        } else if (Integerarray[1].compareTo(Integerarray[0]) > 0 && Integerarray[1].compareTo(Integerarray[2]) > 0) {
             return Integerarray[1];
-        }
-        else{
+        } else {
             return Integerarray[2];
+        }
+    }
+
+    public Float checkMaxFloat(Float[] floatarray) {
+        if (floatarray[0].compareTo(floatarray[1]) > 0 && floatarray[0].compareTo(floatarray[2]) > 0) {
+            return floatarray[0];
+        } else if (floatarray[1].compareTo(floatarray[0]) > 0 && floatarray[1].compareTo(floatarray[2]) > 0) {
+            return floatarray[1];
+        } else {
+            return floatarray[2];
         }
     }
 }
--- src/test/java/com/bridglabz/MaximumTest.java
+++ src/test/java/com/bridglabz/MaximumTest.java
@@ -5,9 +5,16 @@ import org.junit.Test;
 
 public class MaximumTest {
     @Test
-    public void maximunNumber_AtFirstPlace_ReturnMax () {
-        Maximum maximum=new Maximum();
-        int result=maximum.checkMaxInteger(new Integer[]{1,3,2});
-        Assert.assertEquals(3,result);
+    public void maximunNumber_Integer_ReturnMax() {
+        Maximum maximum = new Maximum();
+        int result = maximum.checkMaxInteger(new Integer[]{1, 3, 2});
+        Assert.assertEquals(3, result);
+    }
+
+    @Test
+    public void maximunNumber_Float_ReturnMax() {
+        Maximum maximum = new Maximum();
+        float result = maximum.checkMaxFloat(new Float[]{1.1f, 1.2f, 2.2f});
+        Assert.assertEquals(2.2f, result, 0.0);
     }
 }
