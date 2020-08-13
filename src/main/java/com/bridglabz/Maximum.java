ae77533a2a4eecdd58912ad75b57e9040c963fec
unknown <himanshugharat44@gmail.com>
2020-08-13 07:40:57 -0500

    REFACTORED:Created a generic method for finding maximum

--- src/main/java/com/bridglabz/Maximum.java
+++ src/main/java/com/bridglabz/Maximum.java
@@ -2,32 +2,15 @@ package com.bridglabz;
 
 
 public class Maximum {
-    public Integer checkMaxInteger(Integer[] Integerarray) {
-        if (Integerarray[0].compareTo(Integerarray[1]) > 0 && Integerarray[0].compareTo(Integerarray[2]) > 0) {
-            return Integerarray[0];
-        } else if (Integerarray[1].compareTo(Integerarray[0]) > 0 && Integerarray[1].compareTo(Integerarray[2]) > 0) {
-            return Integerarray[1];
+    public <E extends Comparable> E checkMax(E[] array) {
+        if (array[0].compareTo(array[1]) > 0 && array[0].compareTo(array[2]) > 0) {
+            return array[0];
+        } else if (array[1].compareTo(array[0]) > 0 && array[1].compareTo(array[2]) > 0) {
+            return array[1];
         } else {
-            return Integerarray[2];
+            return array[2];
         }
     }
 
-    public Float checkMaxFloat(Float[] floatarray) {
-        if (floatarray[0].compareTo(floatarray[1]) > 0 && floatarray[0].compareTo(floatarray[2]) > 0) {
-            return floatarray[0];
-        } else if (floatarray[1].compareTo(floatarray[0]) > 0 && floatarray[1].compareTo(floatarray[2]) > 0) {
-            return floatarray[1];
-        } else {
-            return floatarray[2];
-        }
-    }
-    public String checkMaxString(String[] stringarray) {
-        if (stringarray[0].compareTo(stringarray[1]) > 0 && stringarray[0].compareTo(stringarray[2]) > 0) {
-            return stringarray[0];
-        } else if (stringarray[1].compareTo(stringarray[0]) > 0 && stringarray[1].compareTo(stringarray[2]) > 0) {
-            return stringarray[1];
-        } else {
-            return stringarray[2];
-        }
-    }
+
 }
--- src/test/java/com/bridglabz/MaximumTest.java
+++ src/test/java/com/bridglabz/MaximumTest.java
@@ -7,21 +7,21 @@ public class MaximumTest {
     @Test
     public void maximunNumber_Integer_ReturnMax() {
         Maximum maximum = new Maximum();
-        int result = maximum.checkMaxInteger(new Integer[]{1, 3, 2});
+        int result = maximum.checkMax(new Integer[]{1, 3, 2});
         Assert.assertEquals(3, result);
     }
 
     @Test
     public void maximunNumber_Float_ReturnMax() {
         Maximum maximum = new Maximum();
-        float result = maximum.checkMaxFloat(new Float[]{1.1f, 1.2f, 2.2f});
+        float result = maximum.checkMax(new Float[]{1.1f, 1.2f, 2.2f});
         Assert.assertEquals(2.2f, result, 0.0);
     }
 
     @Test
     public void maximunNumber_String_ReturnMax() {
         Maximum maximum = new Maximum();
-        String result = maximum.checkMaxString(new String[]{"apple","banana","orange"});
+        String result = maximum.checkMax(new String[]{"apple","banana","orange"});
         Assert.assertEquals("orange", result);
     }
 }
