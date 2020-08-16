8a5a7c7edc08ce60d14df459038ca1439a82206f
unknown <himanshugharat44@gmail.com>
2020-08-16 05:09:43 -0500

    ADDED:now we get the max value printed

--- src/main/java/com/bridglabz/Maximum.java
+++ src/main/java/com/bridglabz/Maximum.java
@@ -1,21 +1,35 @@
 package com.bridglabz;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-public class Maximum <E extends Comparable>{
+public class Maximum<E extends Comparable> {
     E firstValue;
     E secondValue;
     E thirdValue;
     E[] array;
+    E max;
+
     public Maximum(E firstValue, E secondValue, E thirdValue) {
         this.firstValue = firstValue;
         this.secondValue = secondValue;
         this.thirdValue = thirdValue;
+
     }
 
     public Maximum(E[] array) {
-        this.array=array;
+        this.array = array;
     }
 
+    public E checkMaxParameters() {
+        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(secondValue) > 0) {
+            return firstValue;
+        } else if (secondValue.compareTo(firstValue) > 0 && secondValue.compareTo(thirdValue) > 0) {
+            return secondValue;
+        } else {
+            return thirdValue;
+        }
+    }
     public E checkMax() {
         if (array[0].compareTo(array[1]) > 0 && array[0].compareTo(array[2]) > 0) {
             return array[0];
@@ -25,6 +39,21 @@ public class Maximum <E extends Comparable>{
             return array[2];
         }
     }
+    public E printMax(int parameter) {
+        if (parameter <= 3)
+            checkMax();
+        else {
+            Arrays.sort(array);
+        }
+        max=array[0];
+        if (max.compareTo(array[array.length - 1]) < 0)
+            max = array[array.length - 1];
+
+        return max;
+    }
+
 
 
 }
+
+
--- src/test/java/com/bridglabz/MaximumTest.java
+++ src/test/java/com/bridglabz/MaximumTest.java
@@ -19,9 +19,17 @@ public class MaximumTest {
     }
 
     @Test
-    public void maximunNumber_String_ReturnMax() {
-        Maximum maximum = new Maximum(new String[]{"apple","banana","orange"});
+    public void maximumNumber_String_ReturnMax() {
+        Maximum maximum = new Maximum(new String[]{"apple", "banana", "orange"});
         Comparable result = maximum.checkMax();
         Assert.assertEquals("orange", result);
+    }
+
+    @Test
+    public void maximumNumber_Integer_ReturnMax() {
+        Maximum maximum=new Maximum(new Integer[]{1,2,3,4,5,6});
+        Comparable result= maximum.printMax(6);
+        Assert.assertEquals(6,result);
+
     }
 }
